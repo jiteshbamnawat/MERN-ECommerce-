be7f8a1a403bdfa629a0d2ab465e9a2a4302d4a4
Jitesh Meena <88947409+jiteshbamnawat@users.noreply.github.com>
2023-10-05 07:11:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import morgan from 'morgan'
+import authRoutes from './routes/authRoute.js'
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from './routes/productRoutes.js'
+import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
+//configure ev
+dotenv.config()
+
+//database config
+connectDB()
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//rest object
+const app = express()
+
+//middleware
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+
+//routes
+app.use('/api/v1/auth',authRoutes)
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+//rest api
+app.use('*', function(req, res) {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+//port
+const PORT = process.env.PORT || 8080
+
+//run listen
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`)
+})
